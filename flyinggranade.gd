cf0b2b242e4002855111af1797aa04deefa8bf1a
unknown <pizduchab@gmail.com>
2023-05-03 18:48:30 +0200

    this one is the one with the things from previous one, i hope

---
+++ flyinggranade.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var speed = 720
+var thrown_by: Node = null
+
+func _physics_process(delta):
+	$granade.position.x += speed * delta
+
+	if $granade.position.x >= $impact.position.x:
+		var players_hit = $impact.get_overlapping_bodies()
+		for player in players_hit:
+			player.hit_by_granade(self)
+		
+		get_parent().remove_child(self)
